c84bd638fde56c9d73abc79facf2426c4af95ea6
juanjose <jarangoh@unal.edu.co>
2026-06-10 21:09:56 -0500

    Ejercicio en Java con primer acercamiento

--- ExercisesJava/src/Ejercicios/SumMultiple.java
+++ ExercisesJava/src/Ejercicios/SumMultiple.java
@@ -1,5 +1,17 @@
 package Ejercicios;
 
 public class SumMultiple {
-
+	public static void main(String[] args) {
+		sumMultiple(7);
+	}
+	public static int sumMultiple(int n) {
+		int contador = 0;
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+				contador+=i;
+			}
+		}
+		System.out.println(contador);
+		return contador;
+	}
 }
