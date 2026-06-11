1839c1cfd443958b6a11f0619908cafad95ec075
juanjose <jarangoh@unal.edu.co>
2026-06-10 20:09:45 -0500

    Solucion optima en python con formula matematica

--- ExercisesPython/SumMultiples.py
+++ ExercisesPython/SumMultiples.py
@@ -8,3 +8,24 @@ class Solution:
         return contador
 Solucion = Solution()
 Solucion.sumOfMultiples(7)
+#Solucion con fórmula de la suma de una progresión aritmética
+#Solucion en O(1) en tiempo y espacio
+def sumOfMultiples(n: int) -> int:
+    def sum_divisible_by(target: int) -> int:
+        # Número de términos divisibles por 'target' hasta n
+        m = n // target
+        # Fórmula de la suma aritmética: target * (1 + 2 + ... + m)
+        return target * m * (m + 1) // 2
+
+    # Principio de Inclusión-Exclusión
+    print(
+        sum_divisible_by(3) + sum_divisible_by(5) + sum_divisible_by(7)
+        - sum_divisible_by(15) - sum_divisible_by(21) - sum_divisible_by(35)
+        + sum_divisible_by(105)
+    )
+    return (
+        sum_divisible_by(3) + sum_divisible_by(5) + sum_divisible_by(7)
+        - sum_divisible_by(15) - sum_divisible_by(21) - sum_divisible_by(35)
+        + sum_divisible_by(105)
+    )
+sumOfMultiples(7)
